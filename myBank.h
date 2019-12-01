ae9232e3066c3889dc6b757e31b1f581237d0869
amit1021 <57723590+amit1021@users.noreply.github.com>
2019-12-01 15:01:38 +0200

    Create myBank.h

---
+++ myBank.h
@@ -0,0 +1,11 @@
+
+void open(double Bank [2][50]);
+void balance(double Bank [2][50]);
+void deposit(double Bank [2][50]);
+void withdrawal(double Bank [2][50]);
+void close(double Bank[2][50]);
+void inseret(double Bank [2][50]);
+void print(double Bank[2][50]);
+void ex(double Bank[2][50]);
+
+int main();
